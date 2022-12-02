ef39b2b85ed7840a4725f17af5c600dc26e947b1
rolandpo <rolopod@gmail.com>
2022-12-02 16:38:33 +0100

    fix article content

--- src/components/Article.js
+++ src/components/Article.js
@@ -27,7 +27,7 @@ const Article = ({ article, navLinks }) => {
         <>
           <Header mappedLinks={navLinks} webProperty="ousd" />
           <div className='bg-[#141519] px-8 md:px-16 lg:px-[134px]'>
-            <div className="max-w-screen-lg mx-auto">
+            <div className="max-w-[943px] mx-auto">
               <Link href={'/blog'} className="inline-block p-[1px] rounded-full gradient2">
                 <div className='w-full h-full px-4 md:px-6 py-1.5 text-center rounded-full bg-[#141519]'>
                   <div className='flex flex-row justify-between space-x-3 md:space-x-5'>
@@ -43,21 +43,21 @@ const Article = ({ article, navLinks }) => {
                 </div>
               </Link>
             </div>
-            <div className="max-w-screen-lg mx-auto mt-6 md:mt-12">
-              <Typography.H4 as="h1" style={{ fontSize: '2.75rem', lineHeight: '3.75rem' }}>{article.title}</Typography.H4>
+            <div className="max-w-[943px] mx-auto mt-6 md:mt-12">
+              <Typography.H4 as="h1" className='text-[24px] md:text-[56px] leading-[32px] md:leading-[72px]'>{article.title}</Typography.H4>
             </div>
-            <div className="max-w-screen-lg mx-auto mt-3 md:mt-6">
+            <div className="max-w-[943px] mx-auto mt-3 md:mt-6">
               <Typography.Body3 className='text-[14px] md:text-[16px] text-[#b5beca]'>
                 <Moment format="MMM Do YYYY">{article.published_at}</Moment>
               </Typography.Body3>
             </div>
           </div>
           <div className='gradient5 px-4 md:px-16 lg:px-[134px]'>
-            <div className="relative max-w-screen-lg mx-auto mt-8 md:mt-16 rounded-2xl">
+            <div className="relative max-w-[943px] mx-auto mt-8 md:mt-16 rounded-2xl">
               {imageUrl && (
                 <div
                   id="banner"
-                  className="rounded-t-2xl overflow-hidden"
+                  className="rounded-2xl overflow-hidden"
                   data-src={imageUrl}
                   data-srcset={imageUrl}
                 >
@@ -75,8 +75,9 @@ const Article = ({ article, navLinks }) => {
             </div>
           </div>
           <div className='bg-[#1e1f25] px-6 md:px-16 lg:px-[134px] pt-8 md:pt-16 pb-10 md:pb-[120px]'>
-            <div className={`max-w-screen-lg mx-auto ${styles.article}`}>
+            <div className={`max-w-[943px] mx-auto`}>
               <div
+                className={`font-sansSailec ${styles.article}`}
                 dangerouslySetInnerHTML={{
                   __html: sanitizeHtml(he.decode(article.body), sanitizationOptions),
                 }}
