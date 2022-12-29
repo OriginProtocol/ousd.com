a843a613815b195a5ea1b042ef1b3af44265f90f
rolandpo <rolopod@gmail.com>
2022-12-29 15:01:26 +0100

    add gtm

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GTM_ID } from '../lib/gtm'
 
 class MyDocument extends Document {
   render() {
@@ -13,6 +14,14 @@ class MyDocument extends Document {
           />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
