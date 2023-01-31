44af7793764567f1f08150e25277227abd9dbfdd
Hrik Bhowal <hrik.bhowal@gmail.com>
2023-01-31 10:39:58 -0800

    move files out of pages directory

--- src/ogv-dashboard/sections/StakingBanner.tsx
+++ src/ogv-dashboard/sections/StakingBanner.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { Section } from "../components";
-import { assetRootPath } from "../../../src/utils/image";
+import { assetRootPath } from "../../utils/image";
 import { Button } from "@originprotocol/origin-storybook";
 import { smSize } from "../constants";
 
