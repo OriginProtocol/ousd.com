a116a16974057dfae8ba36484123c12ba239847e
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-03-30 22:31:46 +0400

    Add log

--- pages/ogv-dashboard.tsx
+++ pages/ogv-dashboard.tsx
@@ -49,6 +49,7 @@ import {
   TopExchanges,
   OgvStakingStats,
 } from "../src/ogv-dashboard/sections";
+import Error from "./404";
 
 ChartJS.register(
   CategoryScale,
@@ -75,6 +76,8 @@ const ogvStakingCache: {
 // Dependency flow: constants, types -> chart-configs, data, utils -> components -> sections -> index.tsx (this file)
 
 const OgvDashboard = ({
+  //@ts-ignore
+  error,
   seo,
   navLinks,
   priceData24H,
@@ -96,6 +99,9 @@ const OgvDashboard = ({
   const stakingApy =
     getRewardsApy(100 * 1.8 ** (48 / 12), 100, parseFloat(totalVeSupply)) || 0;
 
+    if (error) {
+      return  <Error navLinks={[]} />;
+    }
   return (
     <>
       <Head>
@@ -151,155 +157,166 @@ const OgvDashboard = ({
 export const getStaticProps: GetStaticProps = async (): Promise<{
   props: DashProps;
 }> => {
-  const seoResPromise = fetchAPI("/ousd/page/en/%2Fogv-dashboard");
-  const navResPromise = fetchAPI("/ousd-nav-links", {
-    populate: {
-      links: {
-        populate: "*",
+  try {
+      
+    const seoResPromise = fetchAPI("/ousd/page/en/%2Fogv-dashboard");
+    const navResPromise = fetchAPI("/ousd-nav-links", {
+      populate: {
+        links: {
+          populate: "*",
+        },
       },
-    },
-  });
+    });
 
-  const rawData24HPromise = getOGVPriceData(1);
-  const rawData7DPromise = getOGVPriceData(7);
-  const rawData30DPromise = getOGVPriceData(30);
-  const rawData365DPromise = getOGVPriceData(365);
+    const rawData24HPromise = getOGVPriceData(1);
+    const rawData7DPromise = getOGVPriceData(7);
+    const rawData30DPromise = getOGVPriceData(30);
+    const rawData365DPromise = getOGVPriceData(365);
 
-  const currentPriceDataPromise = fetch(
-    "https://api.coingecko.com/api/v3/simple/price?ids=origin-dollar-governance&vs_currencies=usd&include_market_cap=true&include_24hr_change=true&precision=full"
-  );
+    const currentPriceDataPromise = fetch(
+      "https://api.coingecko.com/api/v3/simple/price?ids=origin-dollar-governance&vs_currencies=usd&include_market_cap=true&include_24hr_change=true&precision=full"
+    );
 
-  const provider = new providers.JsonRpcProvider(
-    process.env.NEXT_PUBLIC_ETHEREUM_RPC_PROVIDER
-  );
-  const OGV = new ethers.Contract(
-    "0x9c354503c38481a7a7a51629142963f98ecc12d0",
-    ogvAbi,
-    provider
-  );
+    const provider = new providers.JsonRpcProvider(
+      process.env.NEXT_PUBLIC_ETHEREUM_RPC_PROVIDER
+    );
+    const OGV = new ethers.Contract(
+      "0x9c354503c38481a7a7a51629142963f98ecc12d0",
+      ogvAbi,
+      provider
+    );
 
-  const days = 90;
-  const ethBlocksPerDay = 5760;
-  const currentBlock = await provider.getBlockNumber();
-  const ogvContractAddress = "0x9c354503C38481a7A7a51629142963F98eCC12D0";
+    const days = 90;
+    const ethBlocksPerDay = 5760;
+    const currentBlock = await provider.getBlockNumber();
+    const ogvContractAddress = "0x9c354503C38481a7A7a51629142963F98eCC12D0";
 
-  // The slot number of OGVStaking.sol's OGV balance
-  // slither-read-storage 0x7DFAFe7d547Fc9083719D633B9c5f6f542C42c77
-  // --variable-name _balances --key 0x0C4576Ca1c365868E162554AF8e385dc3e7C66D9
-  // --storage-address 0x9c354503C38481a7A7a51629142963F98eCC12D0 --rpc-url
-  // [YOUR_RPC_URL]
-  const stakingContractOgvBalanceSlot = BigNumber.from(
-    "53374832710725341261650961104440469343146346525711704527343957115373724579195"
-  );
+    // The slot number of OGVStaking.sol's OGV balance
+    // slither-read-storage 0x7DFAFe7d547Fc9083719D633B9c5f6f542C42c77
+    // --variable-name _balances --key 0x0C4576Ca1c365868E162554AF8e385dc3e7C66D9
+    // --storage-address 0x9c354503C38481a7A7a51629142963F98eCC12D0 --rpc-url
+    // [YOUR_RPC_URL]
+    const stakingContractOgvBalanceSlot = BigNumber.from(
+      "53374832710725341261650961104440469343146346525711704527343957115373724579195"
+    );
 
-  // The slot number of OGV's total supply
-  // slither-read-storage 0x7DFAFe7d547Fc9083719D633B9c5f6f542C42c77
-  // --variable-name _totalSupply --key 0x0C4576Ca1c365868E162554AF8e385dc3e7C66D9
-  // --storage-address 0x9c354503C38481a7A7a51629142963F98eCC12D0 --rpc-url
-  // [YOUR_RPC_URL]
-  const ogvTotalSupplySlot = BigNumber.from("53");
+    // The slot number of OGV's total supply
+    // slither-read-storage 0x7DFAFe7d547Fc9083719D633B9c5f6f542C42c77
+    // --variable-name _totalSupply --key 0x0C4576Ca1c365868E162554AF8e385dc3e7C66D9
+    // --storage-address 0x9c354503C38481a7A7a51629142963F98eCC12D0 --rpc-url
+    // [YOUR_RPC_URL]
+    const ogvTotalSupplySlot = BigNumber.from("53");
 
-  const rawStakingDataPromise = fetchOGVStakingData(
-    days,
-    ethBlocksPerDay,
-    currentBlock,
-    ogvContractAddress,
-    stakingContractOgvBalanceSlot,
-    ogvTotalSupplySlot
-  );
+    const rawStakingDataPromise = fetchOGVStakingData(
+      days,
+      ethBlocksPerDay,
+      currentBlock,
+      ogvContractAddress,
+      stakingContractOgvBalanceSlot,
+      ogvTotalSupplySlot
+    );
 
-  const totalSupplyPromise: Promise<BigNumber> = OGV.totalSupply();
+    const totalSupplyPromise: Promise<BigNumber> = OGV.totalSupply();
 
-  let [
-    seoRes,
-    navRes,
-    rawData24H,
-    rawData7D,
-    rawData30D,
-    rawData365D,
-    currentPriceData,
-    totalSupply,
-    rawStakingData,
-    ...nonCirculatingBalances
-  ] = await Promise.all([
-    seoResPromise,
-    navResPromise,
-    rawData24HPromise,
-    rawData7DPromise,
-    rawData30DPromise,
-    rawData365DPromise,
-    currentPriceDataPromise,
-    totalSupplyPromise,
-    rawStakingDataPromise,
-    ...nonCirculating.map((e) => OGV.balanceOf(e.address)),
-  ]);
-
-  const nonCirculatingSupply = nonCirculating.map((e, i) => ({
-    ...e,
-    balance: nonCirculatingBalances[i].toString(),
-  }));
-
-  currentPriceData = await currentPriceData.json();
-
-  const seo: PageSeo = formatSeo(seoRes?.data) as PageSeo;
-  const navLinks: Link[] = transformLinks(navRes.data) as Link[];
-
-  const { usd: currentPrice, usd_24h_change: change24H } =
-    currentPriceData["origin-dollar-governance"];
-
-  const circulatingSupply = calculateCirculatingSupply(
-    totalSupply,
-    nonCirculatingSupply
-  );
-
-  // Calculate market cap
-  rawData24H.marketCaps = rawData24H.prices.map((p: number) =>
-    calculateMarketCap(circulatingSupply, p)
-  );
-  rawData7D.marketCaps = rawData7D.prices.map((p: number) =>
-    calculateMarketCap(circulatingSupply, p)
-  );
-  rawData30D.marketCaps = rawData30D.prices.map((p: number) =>
-    calculateMarketCap(circulatingSupply, p)
-  );
-  rawData365D.marketCaps = rawData365D.prices.map((p: number) =>
-    calculateMarketCap(circulatingSupply, p)
-  );
-
-  const { priceData24H, marketCapData24H } = get24HChartData(rawData24H);
-
-  const currentMarketCap = calculateMarketCap(circulatingSupply, currentPrice);
-
-  const { lastUpdated, data, ttl } = ogvStakingCache;
-
-  // 24HR ttl
-  const cacheHit = lastUpdated ? Date.now() < lastUpdated + ttl : false;
-  let stakingData: ChartData<"line", (number | Point)[], unknown>;
-  if (cacheHit) stakingData = data;
-  else {
-    stakingData = getStakingChartData(rawStakingData, days);
-    ogvStakingCache.lastUpdated = Date.now();
-    ogvStakingCache.data = stakingData;
-  }
-
-  return {
-    props: {
-      seo,
-      navLinks,
-      priceData24H,
-      marketCapData24H,
+    let [
+      seoRes,
+      navRes,
+      rawData24H,
       rawData7D,
       rawData30D,
       rawData365D,
-      stakingData,
-      currentPrice,
-      currentMarketCap,
-      change24H,
-      totalSupply: totalSupply.toString(),
-      doughnutData,
-      nonCirculatingSupply,
-    },
-  };
+      currentPriceData,
+      totalSupply,
+      rawStakingData,
+      ...nonCirculatingBalances
+    ] = await Promise.all([
+      seoResPromise,
+      navResPromise,
+      rawData24HPromise,
+      rawData7DPromise,
+      rawData30DPromise,
+      rawData365DPromise,
+      currentPriceDataPromise,
+      totalSupplyPromise,
+      rawStakingDataPromise,
+      ...nonCirculating.map((e) => OGV.balanceOf(e.address)),
+    ]);
+
+    const nonCirculatingSupply = nonCirculating.map((e, i) => ({
+      ...e,
+      balance: nonCirculatingBalances[i].toString(),
+    }));
+
+    currentPriceData = await currentPriceData.json();
+
+    const seo: PageSeo = formatSeo(seoRes?.data) as PageSeo;
+    const navLinks: Link[] = transformLinks(navRes.data) as Link[];
+
+    const { usd: currentPrice, usd_24h_change: change24H } =
+      currentPriceData["origin-dollar-governance"];
+
+    const circulatingSupply = calculateCirculatingSupply(
+      totalSupply,
+      nonCirculatingSupply
+    );
+
+    // Calculate market cap
+    rawData24H.marketCaps = rawData24H.prices.map((p: number) =>
+      calculateMarketCap(circulatingSupply, p)
+    );
+    rawData7D.marketCaps = rawData7D.prices.map((p: number) =>
+      calculateMarketCap(circulatingSupply, p)
+    );
+    rawData30D.marketCaps = rawData30D.prices.map((p: number) =>
+      calculateMarketCap(circulatingSupply, p)
+    );
+    rawData365D.marketCaps = rawData365D.prices.map((p: number) =>
+      calculateMarketCap(circulatingSupply, p)
+    );
+
+    const { priceData24H, marketCapData24H } = get24HChartData(rawData24H);
+
+    const currentMarketCap = calculateMarketCap(circulatingSupply, currentPrice);
+
+    const { lastUpdated, data, ttl } = ogvStakingCache;
+
+    // 24HR ttl
+    const cacheHit = lastUpdated ? Date.now() < lastUpdated + ttl : false;
+    let stakingData: ChartData<"line", (number | Point)[], unknown>;
+    if (cacheHit) stakingData = data;
+    else {
+      stakingData = getStakingChartData(rawStakingData, days);
+      ogvStakingCache.lastUpdated = Date.now();
+      ogvStakingCache.data = stakingData;
+    }
+
+    return {
+      props: {
+        seo,
+        navLinks,
+        priceData24H,
+        marketCapData24H,
+        rawData7D,
+        rawData30D,
+        rawData365D,
+        stakingData,
+        currentPrice,
+        currentMarketCap,
+        change24H,
+        totalSupply: totalSupply.toString(),
+        doughnutData,
+        nonCirculatingSupply,
+      },
+    };
+  } catch (err) {
+    console.trace(err)
+    return {
+      props: {
+        //@ts-ignore
+        error: true
+      }
+    }
+  }
 };
 
 export default OgvDashboard;
