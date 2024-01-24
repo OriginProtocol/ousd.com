8da063d92a77041f2c78b5d5c49e781d718c41bd
dsadsadsadsada <157620767+dsadsadsadsada@users.noreply.github.com>
2024-01-24 17:07:54 +0100

    Update utils.ts

--- src/analytics/utils.ts
+++ src/analytics/utils.ts
@@ -6,7 +6,7 @@ import { slice } from 'lodash'
 export const backingTokens = {
   DAI: {
     label: 'DAI (DAI)',
-    logoSrc: '/images/dai-logo.svg',
+    logoSrc: '/images/dai-logos.svg',
     color: '#FBC247'
   },
   USDC: {
